7aaca31d35de89a327c784632975276874dbc75b
bayegb <fllbay001@myuct.ac.za>
2024-04-17 14:20:53 -0700

    Added the classifier.py file- extracted MNIST data, seperated it into training, validation and test sets, and reshaped it into vectors

---
+++ classifier.py
@@ -0,0 +1,80 @@
+import torch
+import torch.nn as nn
+from torchvision import datasets
+from matplotlib.pyplot import imshow
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+
+
+# Load MNIST from file
+DATA_DIR = "."
+download_dataset = False
+
+train_mnist = datasets.MNIST(DATA_DIR, train=True, download=download_dataset)
+test_mnist = datasets.MNIST(DATA_DIR, train=False, download=download_dataset)
+
+
+# Create variables for MNIST data
+X_train = train_mnist.data.float()
+y_train = train_mnist.targets
+X_test = test_mnist.data.float()
+y_test = test_mnist.targets
+
+# Sample random indices for validation
+test_size = X_test.shape[0]
+indices = np.random.choice(X_train.shape[0], test_size, replace=False)
+
+# Create validation set
+X_valid = X_train[indices]
+y_valid = y_train[indices]
+
+# Remove validation set from training set
+X_train = np.delete(X_train, indices, axis=0)
+y_train = np.delete(y_train, indices, axis=0)
+
+print(X_train.shape)
+print(y_train.shape)
+print(X_valid.shape)
+print(y_valid.shape)
+print(X_test.shape)
+print(y_test.shape)
+
+# Create data loaders
+batch_size = 64
+
+train_dataset = torch.utils.data.TensorDataset(X_train, y_train)
+val_dataset = torch.utils.data.TensorDataset(X_valid, y_valid)
+test_dataset = torch.utils.data.TensorDataset(X_test, y_test)
+
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+val_loader = torch.utils.data.DataLoader(dataset=val_dataset, batch_size=batch_size, shuffle=False)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+# reshape the data from matrices to vectors
+X_train = X_train.reshape(-1, 28*28)
+X_valid = X_valid.reshape(-1, 28*28)
+X_test = X_test.reshape(-1, 28*28)
+
+print(X_train.shape)
+print(X_valid.shape)
+print(X_test.shape)
+
+# Which model to use?
+# How to go through the layers in an effort to train the model?
+# How to do validation ?
+# How to implement reguralisation?
+
+
+# Seperate the X_train and X_Test into vectors of 784 elements, pixel values
+#X_train = X_train.reshape(-1, 28*28)
+#X_test = X_test.reshape(-1, 28*28)
+
+#print(X_train.shape)
+#print(X_test.shape)
+
+# use numpy array for labels and vectors of the images 
+
+# go through the neural network using ANN - feedforward algorithm
+# ensure last activation function is able to deduce the digit?  
